c6a31d860c67df674adde004c30fe55801a2210b
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-06-13 10:29:03 -0300

    First steps

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ Component } from 'react';
+import { StyleSheet, Text, View, Animated } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default class App extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      LargAnimada: new Animated.Value(150),
+      AltAnimada: new Animated.Value(50)
+    }
+
+    Animated.timing(
+      this.state.LargAnimada,
+      {
+        toValue: 300,
+        duration: 2000
+      }
+    ).start();
+  }
+
+render(){
+  return(
+    <View style={StyleSheet.container}>
+      <Animated.View style={{width: this.state.LargAnimada, height: this.state.AltAnimada, backgroundColor: '#4169E1', justifyContent: 'center'}}>
+        <Text style={{color: '#FFFFFF', fontSize: 22, textAlign: 'center'}}>Carregando ...</Text>
+      </Animated.View>
     </View>
-  );
-}
+   )
+  }  
+ }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
+    alignItems: 'center',
+  }
 });
