2c06bfa093f5afd3f7bdbc312b4089d97ae0531b
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-06-13 12:55:18 -0300

    Using Interpolations

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React,{ Component } from 'react';
 import { StyleSheet, Text, View, Animated, TouchableOpacity } from 'react-native';
 
@@ -6,79 +5,38 @@ export default class App extends Component {
   constructor (props){
     super(props);
     this.state = {
-      LargAnimada: new Animated.Value(150),
-      AltAnimada: new Animated.Value(50)
+      LarAnimada: new Animated.Value(0)
     }
-    this.carregarGrafico = this.carregarGrafico.bind(this);
-
-  }
-  carregarGrafico(){
-
-    Animated.sequence([
-      Animated.timing(
-        this.state.AltAnimada,
-        {
-          toValue: 300,
-          duration: 1000
-        }
-      )
-    ]).start()
+    Animated.timing(
+      this.state.LarAnimada,
+      {
+        toValue:100,
+        duration: 5000
+        
+      }
+    ).start()
   }
 
 render(){
-  return(
-    <View style={{flex: 1}}>
-
-    <View style={{
-      marginTop: 50,
-      height: 80, 
-      alignItems: 'center', 
-      justifyContent: "center" ,
-      flexDirection: 'row',
-      backgroundColor: "#4169E1"
-    }}>
-
-    <TouchableOpacity>
-      <Text style={{ fontSize: 25, color: '#ffffff'}}>
-        Gerar Gráfico
-      </Text>
-    </TouchableOpacity>
-
-    </View>
-
-    <View style={{flex: 1, flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'center'}}>
-
-      <Animated.View style={{width: this.state.LargAnimada,
-                            height: this.state.AltAnimada, 
-                            backgroundColor: '#FF0000', 
-                            justifyContent: 'center', 
-                            borderRadius: 25}}>
-
-
-        <Text style={{color: '#FFFFFF', 
-                      fontSize: 22, 
-                      textAlign: 'center'
-                      }}>R$ 150,00</Text>
-
+  let porcentagemAnimate = this.state.LarAnimada.interpolate({
+    inputRange: [0, 100],
+    outputRange: ["0%", "100%"]
+  })
+  return (
+    <View style={styles.container}>
+      <Animated.View style={{backgroundColor: '#4169E1', width: porcentagemAnimate, height: 25}}>
 
       </Animated.View>
-
-
     </View>
-
-
-
-
-    </View>
-   )
-  }  
+  )
  }
+}
 
 const styles = StyleSheet.create({
   container:{
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     
   }
 });
