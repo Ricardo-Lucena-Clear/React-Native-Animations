32a4dfd5175570a71b4714d4fd4de4baa1d80da9
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-06-13 12:30:40 -0300

    Triggering Animations

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{ Component } from 'react';
-import { StyleSheet, Text, View, Animated } from 'react-native';
+import { StyleSheet, Text, View, Animated, TouchableOpacity } from 'react-native';
 
 export default class App extends Component {
   constructor (props){
@@ -9,34 +9,66 @@ export default class App extends Component {
       LargAnimada: new Animated.Value(150),
       AltAnimada: new Animated.Value(50)
     }
-  
-  Animated.loop(
+    this.carregarGrafico = this.carregarGrafico.bind(this);
+
+  }
+  carregarGrafico(){
+
     Animated.sequence([
       Animated.timing(
-        this.state.LargAnimada,
+        this.state.AltAnimada,
         {
-          toValue: 200,
-          duration: 700
-        }
-      ),
-      Animated.timing(
-        this.state.LargAnimada,
-        {
-          toValue: 150,
-          duration: 700
+          toValue: 300,
+          duration: 1000
         }
       )
-      ])
-  ).start()
-
+    ]).start()
   }
 
 render(){
   return(
-    <View style={StyleSheet.container}>
-      <Animated.View style={{width: this.state.LargAnimada, height: this.state.AltAnimada, backgroundColor: '#4169E1', justifyContent: 'center', borderRadius: 25}}>
-        <Text style={{color: '#FFFFFF', fontSize: 22, textAlign: 'center'}}>Carregando ...</Text>
+    <View style={{flex: 1}}>
+
+    <View style={{
+      marginTop: 50,
+      height: 80, 
+      alignItems: 'center', 
+      justifyContent: "center" ,
+      flexDirection: 'row',
+      backgroundColor: "#4169E1"
+    }}>
+
+    <TouchableOpacity>
+      <Text style={{ fontSize: 25, color: '#ffffff'}}>
+        Gerar Gráfico
+      </Text>
+    </TouchableOpacity>
+
+    </View>
+
+    <View style={{flex: 1, flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'center'}}>
+
+      <Animated.View style={{width: this.state.LargAnimada,
+                            height: this.state.AltAnimada, 
+                            backgroundColor: '#FF0000', 
+                            justifyContent: 'center', 
+                            borderRadius: 25}}>
+
+
+        <Text style={{color: '#FFFFFF', 
+                      fontSize: 22, 
+                      textAlign: 'center'
+                      }}>R$ 150,00</Text>
+
+
       </Animated.View>
+
+
+    </View>
+
+
+
+
     </View>
    )
   }  
